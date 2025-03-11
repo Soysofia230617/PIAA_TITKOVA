2a65f2893046d70a66a6d89bab6181a84473cb0c
Титкова.Софья.3388 <titkovasonya230617@gmail.com>
2025-03-11 21:09:44 +0300

    : : fix

--- piaa_lb1/algorithm.cpp
+++ piaa_lb1/algorithm.cpp
@@ -10,10 +10,12 @@ struct Square {
 };
 
 int cnt = 0;
-bool DEBUG=0;
+bool DEBUG=1;
 
 void print_solution_matrix(int n, const vector<tuple<int, int, int>>& result) {
+    if (DEBUG != 1) return;
 
+    cout << "\n=== Итоговая матрица решения ===\n";
     vector<vector<int>> matrix(n, vector<int>(n, 0));
     int num = 0;
 
@@ -21,7 +23,6 @@ void print_solution_matrix(int n, const vector<tuple<int, int, int>>& result) {
         num += 1;
         int x, y, h;
         tie(x, y, h) = s;
-
         for (int i = x; i < x + h; ++i) {
             for (int j = y; j > y - h; --j) {
                 matrix[i][j] = num;
@@ -31,7 +32,6 @@ void print_solution_matrix(int n, const vector<tuple<int, int, int>>& result) {
 
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < n; ++j) {
-
             if (matrix[i][j] < 10) {
                 cout << matrix[i][j] << "  ";
             } else {
@@ -40,22 +40,28 @@ void print_solution_matrix(int n, const vector<tuple<int, int, int>>& result) {
         }
         cout << endl;
     }
+    cout << "=============================\n";
 }
 
 void rec(vector<int>& diagram, vector<int> marks, vector<Square>& ans) {
     static vector<Square> stack = {};
-    if (DEBUG==1){
+    if (DEBUG == 1) {
         cnt += 1;
-        cout << "IT #" << cnt << '\n';
-
+        cout << "\nИтерация #" << cnt << ":\n";
+        cout << "Текущая диаграмма: ";
+        for (int val : diagram) cout << val << " ";
+        cout << "\nТекущий стек (" << stack.size() << " квадратов):\n";
         for (Square s : stack) {
-            cout << '\t' << s.x << ' ' << s.y << ' ' << s.h << '\n';
+            cout << "\tКвадрат: (" << s.x + 1 << ", " << s.y - s.h + 2 << ") с размером " << s.h << "\n";
         }
     }
 
     if (*max_element(diagram.begin(), diagram.end()) == 0) {
         if (ans.empty() || ans.size() > stack.size()) {
             ans = stack;
+            if (DEBUG == 1) {
+                cout << "\tНайден лучший результат: " << stack.size() << " квадратов\n";
+            }
         }
         return;
     }
@@ -65,7 +71,12 @@ void rec(vector<int>& diagram, vector<int> marks, vector<Square>& ans) {
         corners += (diagram[i] != diagram[i + 1]);
     }
 
+    if (DEBUG == 1) {
+        cout << "\tКоличество углов: " << corners << "\n";
+    }
+
     if (!ans.empty() && stack.size() + corners >= ans.size()) {
+        if (DEBUG == 1) cout << "\tОбрезка ветви: стек + углы >= текущего лучшего результата\n";
         return;
     }
 
@@ -87,10 +98,20 @@ void rec(vector<int>& diagram, vector<int> marks, vector<Square>& ans) {
         for (int h = max_h; h >= 1; --h) {
             if (h > marks[i]) {
                 stack.push_back({i + 1 - h, j, h});
+                if (DEBUG == 1) {
+                    cout << "\tДобавляем квадрат: (" << i + 1 - h + 1 << ", " << j - h + 2
+                         << ") с размером " << h << "\n";
+                }
+
                 int x = marks[i];
                 marks[i] = -1;
                 rec(diagram, marks, ans);
                 marks[i] = x;
+                if (DEBUG == 1) {
+                    cout << "\tУбираем квадрат: (" << stack.back().x + 1 << ", "
+                         << stack.back().y - stack.back().h + 2 << ") с размером "
+                         << stack.back().h << " (не подошел)\n";
+                }
                 stack.pop_back();
             }
             diagram[i + 1 - h] += h;
@@ -108,7 +129,7 @@ int main() {
     vector<Square> ans;
     vector<int> hs;
 
-    for (int h = (n + 1) / 2; h < min((n + 1) / 2 + 5, n-1); ++h) {
+    for (int h = (n + 1) / 2; h < min((n + 1) / 2 + 5, n); ++h) {
         hs.push_back(h);
     }
 
@@ -164,9 +185,9 @@ int main() {
         result.push_back(make_tuple(s.x, s.y, s.h));
     }
 
-    if (DEBUG==1){
-        cout << "Total iterations: " << cnt << '\n';
-        print_solution_matrix(n, result);
+   if (DEBUG==1){
+       cout << "Total iterations: " << cnt << '\n';
+       print_solution_matrix(n, result);
     }
 
     return 0;
